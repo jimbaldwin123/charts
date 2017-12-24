0a77705959d7c305222219687b80474801ad758b
Jim Baldwin <jim@netvendor.net>
2017-12-24 14:43:57 -0800

    add Event model and Guzzle package

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class TimelineController extends Controller
 {
     public function show()
     {
         return view('timeline');
+    }
+
+    public function getWikipediaData()
+    {
+
     }
 }
