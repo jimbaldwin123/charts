b65eafeb82eeaba77222890386295c05dabbbf17
Jim Baldwin <jim@netvendor.net>
2019-10-23 20:12:51 -0700

    webhook

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class ServerController extends Controller
 {
-    public function deploy()
+    public function deploy(Request $request)
     {
+        \Log::debug($request->input());
         $response = [];
         $response[] = shell_exec('cd ' . base_path());
         $response[] = shell_exec('git pull origin master');
