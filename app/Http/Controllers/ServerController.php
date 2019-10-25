fe23f01dff12541df25415f1898e1bce4931226a
Jim Baldwin <jim@netvendor.net>
2019-10-25 14:51:26 -0700

    test deploy

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 
 class ServerController extends Controller
 {
-    public function deploy(Request $request)
+    public function deploy()
     {
-        \Log::debug($request->input());
-        $response = [];
-        $response[] = shell_exec('cd ' . base_path());
-        $response[] = shell_exec('git pull origin master');
+        \Log::debug('DEPLOY ');
+        $response = ['deployed'];
+        // $response[] = shell_exec('cd ' . base_path());
+        // $response[] = shell_exec('git pull origin master');
         print_r($response);
     }
 }
